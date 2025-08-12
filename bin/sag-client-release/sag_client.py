44359691e077b4f078440d4b6c449ab7a1113c4b
ljbrooks <jiang.long@dukekunshan.edu.cn>
2025-08-12 20:32:26 +0800

    Update sag_client.py to not print cmd

--- bin/sag-client-release/sag_client.py
+++ bin/sag-client-release/sag_client.py
@@ -7,7 +7,7 @@ jarfile = cwd + '/'+ 'repolab_sag_client.jar'
 
 cmd = 'java --enable-preview -jar %s ' % jarfile
 #cmd = 'java  -jar %s ' % jarfile
-print(cmd)
+#print(cmd)
 print("Run SAG client: %s" % os.path.abspath(__file__))
 cmd = cmd+  ' '.join(sys.argv[1:])
 print(cmd)
