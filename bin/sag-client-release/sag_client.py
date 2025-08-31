9a862a6bd5128e28a72ee49f70197fbc5043d4ac
ljbrooks <jiang.long@dukekunshan.edu.cn>
2025-08-31 12:57:36 +0800

    up

--- bin/sag-client-release/sag_client.py
+++ bin/sag-client-release/sag_client.py
@@ -3,11 +3,32 @@
 import sys, os
 
 
+def dash(s, msg):
+   print()
+   print(s+':')
+   print('='*(len(s)+1))
+   print(msg+'\n'*2)
+   pass
 
 def is_powershell():
-    return "PSModulePath" in os.environ
+   try:
+      parent = psutil.Process(os.getppid())
+      name = parent.name().lower()
+      return "powershell" in name or "pwsh" in name
+   except Exception:
+      return False
+ 
+try :
+   import psutil
+   if is_powershell():
+      msg = "   Switch to use bash/gitbash to avoid errors"
+      dash("Warning You are in PowerShell", msg)
 
-   
+except:
+   msg = "   Please run `pip install psutil` in your terminal"
+   dash('Installation recommended:', msg)
+
+   pass
 
 
 cwd = os.path.dirname(__file__)
@@ -18,6 +39,8 @@ cmd = 'java --enable-preview -jar %s ' % jarfile
 #cmd = 'java  -jar %s ' % jarfile
 #print(cmd)
 print("Run SAG client: %s" % os.path.abspath(__file__))
+
+
 cmd = cmd+  ' '.join(sys.argv[1:])
 #print(cmd)
 if sys.argv[1] == 'submit':
