777d5fb1dfb00b85bb249e3048fcfdb439d51e83
Omega-98 <Omega980000@gmail.com>
2026-04-26 22:30:16 +0800

    update HW12

--- CS201-S4-SP-2026/HW12/E-4.0.100-Gen_Heap_BST/Gen_Heap_BST.java
+++ CS201-S4-SP-2026/HW12/E-4.0.100-Gen_Heap_BST/Gen_Heap_BST.java
@@ -1,7 +1,26 @@
 public class Gen_Heap_BST {
     // Complete the code here, see README on course website for problem description and instructions.
+    public static String Heap_BST(int lo, int hi){
+        if(lo >= hi){
+            return "";
+        }
+        int N = hi - lo;
+        int L = getLeftSize(N);
+        int rootValue = lo + L;
+        String left = Heap_BST(lo, rootValue);
+        String right = Heap_BST(rootValue + 1, hi);
+        return rootValue + " " + left + right;
+    }
+    public static int getLeftSize(int N){
+        int pow = 0;
+        int P = 1;
+        while (P * 2 <= N + 1) {
+            P = P * 2;
+        }
+        int R = N - (int)(P - 1); // R is the number of the Node in the bottom layer.
+        return P / 2 - 1 + Math.min(R, P/2);
 
-
+    }
 
 
 
